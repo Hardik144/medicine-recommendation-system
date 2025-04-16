8331c163faf5091e94adfa28e1cbb8137ae78364
Hardik144 <patidarhardik81@gmail.com>
2025-04-16 23:20:23 +0530

    Final commit

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def predict():
     return render_template("index.html", result=result, entered_symptoms=symptoms)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5001)
